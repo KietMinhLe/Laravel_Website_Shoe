1ecbc949a5cbde0c4845dfc83bcf972385d39bbb
KietMinhLe <luonjr@gmail.com>
2024-11-10 20:14:28 +0700

    Edit Pages Home

---
+++ resources/views/client/mixins/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="bg-gray-100">
+    <div class="flex space-x-4">
+        <div class="flex-1 px-5">
+            <div class="font-bold text-[25px] py-[20px] ">TEININSNEAKER.COM</div>
+            <div class="text-[20px] text-slate-500">Address: 180 Cao Lỗ, Phường 4, Q8, Hồ Chí Minh.</div>
+            <div class="text-[20px] text-slate-500">Phone: 0903 150 443</div>
+            <div class="text-[20px] text-slate-500">Open: 10am – 9pm (Tất cả các ngày)</div>
+            <div class="text-[20px] text-slate-500">Email: cskh@teininsneaker.com</div>
+        </div>
+        <div class="flex-1 p-4">
+            <div class="font-bold text-[25px] pb-[20px]">CHĂM SÓC KHÁC HÀNG</div>
+            <div class="text-[20px] font-bold pb-2">Hướng dẫn mua hàng</div>
+            <div class="text-[20px] font-bold pb-2">Chính sách bảo mật</div>
+            <div class="text-[20px] font-bold pb-2">Hình thức thanh toán</div>
+            <div class="text-[20px] font-bold pb-2">Vận chuyển và giao nhận</div>
+        </div>
+        <div class="flex-1 p-4">
+            <div class="font-bold text-[25px] pb-[20px]">THÔNG TIN</div>
+            <div class="text-[20px] font-bold pb-2 ">Tài khoản</div>
+            <div class="text-[20px] font-bold pb-2 ">Kiểm tra đơn hàng</div>
+            <div class="text-[20px] font-bold pb-2 ">Đăng kí nhận tin</div>
+        </div>
+        <div class="flex-1 p-4">
+            <div class="font-bold text-[25px] pb-[20px]">SHOP</div>
+            <div class="text-[20px] font-bold">Adidas</div>
+            <div class="text-[20px] font-bold">Nike</div>
+        </div>
+    </div>
+
+</footer>
--- routes/client/index.route.php
+++ routes/client/index.route.php
@@ -1,13 +1,17 @@
 <?php
 
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Route;
+use Laravel\Pail\ValueObjects\Origin\Console;
 
-Route::get('/product', function () {
-    return view('client.layouts.product.default');
+Route::get('/', function () {
+    return view('client.pages.home.index');
+    // return view('client.layouts.default');
 });
-// Route::get('/product', function () {
-//     return view('client.layouts.product.default');
-// });
-// Route::get('/', function () {
-//     return view('client.layouts.product.default');
-// });
+Route::get('/product', function () {
+    return view('client.pages.product.index');
+});
+Route::get('/product/chitiet', function () {
+    // return view('client.layouts.product.default', ['id' => $id]);
+    return view('client.layouts.detail');
+});
